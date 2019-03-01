e1d6a049354966b438e5f04066c566cb6cc3f963
mekhaaa <ahmed.hosnyahmed@student.guc.edu.eg>
2019-03-01 21:06:45 +0200

    lifeCoaches routes

--- server/validations/lifeCoachesValidation.js
+++ server/validations/lifeCoachesValidation.js
@@ -3,9 +3,9 @@ const Joi = require("joi");
 // 2 schemas for password attribute difference when updating
 const createValidation = (request) => {
   const createSchema = {
-    name: Joi.string().min(3).max(30).required(),
+    name: Joi.string().required(),
     dateOfBirth: Joi.date().iso().required(),
-    gender: Joi.string().min(4).max(6).required(),
+    gender: Joi.string().required(),
     hourlyRate:Joi.number(),
     email: Joi.string().email({ minDomainAtoms: 2 }).required(),
     
@@ -16,7 +16,7 @@ const updateValidation = (request) => {
   const updateSchema = {
     name: Joi.string().min(3).max(30).required(),
     dateOfBirth: Joi.date().iso().required(),
-    gender: Joi.string().min(4).max(6).required(),
+    gender: Joi.string().required(),
     hourlyRate:Joi.number(),
     email: Joi.string().email({ minDomainAtoms: 2 }).required(),
     
