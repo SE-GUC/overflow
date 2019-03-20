044f3720401afd8e11b0630234be3ae69d0cd8fe
Omarnael <Omarnael98@gmail.com>
2019-03-21 01:46:50 +0200

    Minor fixes

--- server/routes/api/members.js
+++ server/routes/api/members.js
@@ -2,13 +2,12 @@ const express = require("express");
 const User = require("../../models/User");
 const Member = require("../../models/Member");
 const validator = require("../../validations/memberValidation");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 router.get("/", async (req, res) => {
-  const members = User.find();
-  const filteredMembers = members.filter(
-    member => member.type === "member"
-  );
+  const members = await User.find();
+  const filteredMembers = members.filter(member => member.type === "member");
   return res.json({ data: filteredMembers });
 });
 
@@ -27,7 +26,8 @@ router.post("/create", async (req, res) => {
   //hashing password
   const salt = bcrypt.genSaltSync(10);
   const hashedPassword = bcrypt.hashSync(password, salt);
-  const age = new Date().getFullYear() - dateOfBirth.getFullYear();
+  const { dateOfBirth } = userData;
+  const age = new Date().getFullYear() - new Date(dateOfBirth).getFullYear();
   userData.age = age;
   const member = new Member(userData);
   const user = await User.create({
@@ -43,7 +43,7 @@ router.put("/update/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const query = { _id: id, type: "member" };
-    const user = User.findOne(query);
+    const user = await User.findOne(query);
     const isValidated = validator.updateValidation(req.body);
     if (!user)
       // Bad request if not found
@@ -57,10 +57,10 @@ router.put("/update/:id", async (req, res) => {
     const emailCheck = await User.findOne({ _id: { $ne: id }, email });
     if (emailCheck)
       return res.status(400).json({ error: "Email already exists" });
-  userData.reviews= user.userData.reviews;
-  const { dateOfBirth } = userData;
-  const age = new Date().getFullYear() - dateOfBirth.getFullYear();
-  userData.age = age;
+    userData.reviews = user.userData.reviews;
+    const { dateOfBirth } = userData;
+    const age = new Date().getFullYear() - new Date(dateOfBirth).getFullYear();
+    userData.age = age;
     await User.updateOne(query, { name, email, userData });
     return res.sendStatus(200);
   } catch (error) {
@@ -68,4 +68,4 @@ router.put("/update/:id", async (req, res) => {
     res.sendStatus(400);
   }
 });
- module.exports = router;
+module.exports = router;
