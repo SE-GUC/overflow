3d3af80ea244454091f2dd6606da37c545dad995
Youssefsamy20 <youssefsamy2032@gmail.com>
2019-03-21 18:19:26 +0200

    fixes sub Ids

--- server/models/LifeCoach.js
+++ server/models/LifeCoach.js
@@ -1,25 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const LifeCoachSchema = new Schema({
-  dateOfBirth: {
-    type: Date,
-    required: true
+const LifeCoachSchema = new Schema(
+  {
+    dateOfBirth: {
+      type: Date,
+      required: true
+    },
+    gender: {
+      type: String
+    },
+    joinDate: {
+      type: Date,
+      default: Date.now
+    },
+    hourlyRate: {
+      type: String
+    },
+    age: {
+      type: Number
+    },
+    monthlySlots: {
+      type: [String] //pending Slot Schema
+    }
   },
-  gender: {
-    type: String
-  },
-  joinDate: {
-    type: Date,
-    default: Date.now
-  },
-  hourlyRate: {
-    type: String
-  },
-  age: {
-    type: Number
-  },
-  monthlySlots: {
-    type: [String] //pending Slot Schema
-  }
-});
+  { _id: false }
+);
 module.exports = LifeCoach = mongoose.model("lifeCoaches", LifeCoachSchema);
