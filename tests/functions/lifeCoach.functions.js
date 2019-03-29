fade5d21f54e53a80887741f10b0dd74d0f97fd5
mekhaaa <ahmed.hosnyahmed@student.guc.edu.eg>
2019-03-29 14:46:52 +0200

    LifeCoachTests

---
+++ tests/functions/lifeCoach.functions.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+
+const functions = {
+  getLifeCoaches: async () => {
+    const lifeCoaches = await axios.get("http://localhost:3000/api/users/lifeCoaches");
+    return lifeCoaches;
+  },
+  createLifeCoaches: async body => {
+    const lifeCoaches = await axios({
+      method: "post",
+      url: "http://localhost:3000/api/users/lifeCoaches/create",
+      data: body,
+      headers: { "Content-Type": "application/json" }
+    });
+    return lifeCoaches;
+  },
+  updatelifeCoach: async (id, body) => {
+    await axios({
+      method: "put",
+      url: "http://localhost:3000/api/users/lifeCoaches/update/" + id,
+      data: body,
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+};
+module.exports = functions;
