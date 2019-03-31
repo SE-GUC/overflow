934f29e80263ca3039e37a20ed72164624d0764c
PhilipMouris <philip.sidhom@student.guc.edu.eg>
2019-03-31 13:36:05 +0200

    removes max constraints in validations

--- validations/partnerValidation.js
+++ validations/partnerValidation.js
@@ -3,10 +3,10 @@ const Joi = require("joi");
 // 2 schemas for password attribute difference when updating
 const createValidation = request => {
   const createSchema = {
-    name: Joi.string().min(3).max(30).required(),
-    address: Joi.string().min(3).max(50),
+    name: Joi.string().min(3).required(),
+    address: Joi.string().min(3),
     email: Joi.string().email({ minDomainAtoms: 2 }).required(),
-    fax: Joi.string().min(3).max(50),
+    fax: Joi.string().min(3),
     phone: Joi.number(),
     partners: Joi.array().allow(null),
     members: Joi.array().allow(null),
@@ -17,10 +17,10 @@ const createValidation = request => {
 };
 const updateValidation = request => {
   const updateSchema = {
-    name: Joi.string().min(3).max(30).required(),
-    address: Joi.string().min(3).max(50),
+    name: Joi.string().min(3).required(),
+    address: Joi.string().min(3),
     email: Joi.string().email({ minDomainAtoms: 2 }).required(),
-    fax: Joi.string().min(3).max(50),
+    fax: Joi.string().min(3),
     phone: Joi.number(),
     partners: Joi.array().allow(null),
     members: Joi.array().allow(null),
