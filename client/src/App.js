1c86642d4fc047596f1bfdba624621b60c12c566
Streakfull <yosho98@hotmail.com>
2019-04-06 22:12:00 +0200

    adds basic structure and search

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-// import VacancyPage from "../pages/VacancyPage";
+import { Responsive } from "semantic-ui-react";
+import { withRouter } from "react-router-dom";
 class App extends Component {
   render() {
-    return
+    return <h1> Overflow Test </h1>;
   }
 }
 
-export default App;
+export default withRouter(App);
