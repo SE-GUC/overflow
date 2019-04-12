d9be250d8f5210573998c2365678c2cbef55e27c
Streakfull <elshafie.youssef@gmail.com>
2019-04-12 13:32:40 +0200

    pre merge conflicts

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,11 +4,28 @@ import Vacancy from "./pages/Vacancy";
 import SignUp from "./pages/SignUp";
 import Vacancies from "./pages/Vacancies";
 import CreateVacancy from "./pages/CreateVacancy";
+import Members from "./pages/Members";
+import Partners from "./pages/Partners";
+import LifeCoaches from "./pages/LifeCoaches";
+
 export default () => {
   return [
-    <Route exact path="/Vacancy/:id" component={Vacancy} />,
-    <Route exact path="/SignUp" component={SignUp} />,
-    <Route path="/Vacancies" component={Vacancies} />,
-    <Route exact path="/CreateVacancy/:partnerId" component={CreateVacancy} />
+    <Route key={"/Vacancy"} exact path="/Vacancy/:id" component={Vacancy} />,
+    <Route key={"/SignUp"} exact path="/SignUp" component={SignUp} />,
+    <Route key={"/Vacancies"} path="/Vacancies" component={Vacancies} />,
+    <Route
+      key={"/CreateVacancy"}
+      exact
+      path="/CreateVacancy/:partnerId"
+      component={CreateVacancy}
+    />,
+    <Route key={"/Members"} exact path="/Members" component={Members} />,
+    <Route key={"/Partners"} exact path="/Partners" component={Partners} />,
+    <Route
+      key={"/lifeCoaches"}
+      exact
+      path="/LifeCoaches"
+      component={LifeCoaches}
+    />
   ];
 };
