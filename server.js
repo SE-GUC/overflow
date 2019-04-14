3984fc597f92a8b1d4ffcee63b0cdb38f65a7949
PhilipMouris <philip.sidhom@student.guc.edu.eg>
2019-04-14 22:57:39 +0200

    adds configurations for deployment

--- server.js
+++ server.js
@@ -56,5 +56,9 @@ app.use("/", (req, res) => {
 });
 recommender.setVacanciesProperties();
 recommender.setMemberProperties();
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server up and running on port ${port}`));
