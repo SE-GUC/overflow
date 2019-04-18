66ac4d7acd4776310d470d1bb4b94047e3019b4b
PhilipMouris <philip.sidhom@student.guc.edu.eg>
2019-04-18 21:45:06 +0200

    adds homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,54 +1,17 @@
 import React, { Component } from "react";
-import { List, Header, Grid } from "semantic-ui-react";
+import "../styling/Homepage.css";
+import Landing from "../components/homepage/Landing";
+import Partners from "../components/homepage/Partners";
+import Info from "../components/homepage/Info";
 
 export default class HomePage extends Component {
   render() {
     return (
-      <Grid style={{ marginTop: "5em", marginLeft: "2em" }}>
-        <Grid.Row>
-          <Header textAlign="center">Overflow Team Notes</Header>
-        </Grid.Row>
-        <Grid.Row>
-          <List ordered>
-            <List.Item>
-              Still in progress Items (not deployed )
-              <List.List>
-                <List.Item>Landing page (will be here)</List.Item>
-                <List.Item>Member profile page</List.Item>
-                <List.Item>Some admin privileges</List.Item>
-                <List.Item>
-                  Recommendations using recombee API (finished backend,not
-                  integrated on the front-end yet)
-                </List.Item>
-                <List.Item>
-                  Notifications using google firebase (finished backend,not
-                  integrated on the front-end yet)
-                </List.Item>
-                <List.Item>Deleting Profiles</List.Item>
-                <List.Item>Editing Profiles</List.Item>
-                <List.Item>Forgot Password & changing Password</List.Item>
-              </List.List>
-            </List.Item>
-            <List.Item>
-              Some Account credentials:
-              <List.List>
-                <List.Item>
-                  Member: tristana@hotmail.com, password:test
-                </List.Item>
-                <List.Item>Partner: apple@hotmail.com, password:test</List.Item>
-                <List.Item>Admin: philip@admin.com, password:test</List.Item>
-                <List.Item>
-                  LifeCoach: philip@gotmail.com, password:test
-                </List.Item>
-              </List.List>
-            </List.Item>
-            <List.Item>All images are currently placeholders</List.Item>
-            <List.Item>
-              This website is fully resposnive (works on all screen sizes)
-            </List.Item>
-          </List>
-        </Grid.Row>
-      </Grid>
+      <div>
+        <Landing />
+        <Partners />
+        <Info />
+      </div>
     );
   }
 }
